5e85aad94f0da07d05b9f7fb8cfe14f24433b2d5
Iqbal Samin Prithul <Prithul0218@gmail.com>
2021-03-21 14:24:38 +0600

    Added Bluetooth Functionality
    * Completed the LCD menu
    * Added a random password generator option
    * Added a lock screen at the beginning of the code and on timeout
    * Added Bluetooth module HC-05, used to unlock the device with phone

--- sketch_orpheus_password_manager/sketch_orpheus_password_manager.ino
+++ sketch_orpheus_password_manager/sketch_orpheus_password_manager.ino
@@ -7,32 +7,31 @@
 #include "AESLib.h"
 #include <EEPROM.h>
 #include <LiquidCrystal_I2C.h>
-#include <rotary.h>                 // rotary handler
+#include <rotary.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 #define PINB 4
 #define PINA 5
 #define PUSHB 6
 
-// Initialize the Rotary object
-// Rotary(Encoder Pin 1, Encoder Pin 2, Button Pin) Attach center to ground
-Rotary r = Rotary(PINA, PINB, PUSHB);        // there is no must for using interrupt pins !!
 
-int columnsLCD = 20;
-char* main_menu[] = {"Use a Password", "Generate a Password"};
+Rotary r = Rotary(PINA, PINB, PUSHB);
 
-char* MenuLine[] = {"Google", "repl.it", "Twitter", "Instagram", "Instructables", "Pinterest", "Facebook"};
+const byte columnsLCD = 20;
+
+char* MenuLine[] = {"Google", "repl.it", "Twitter", "Instagram", "Instructables", "Pinterest", "Generate Password"};
 
 int MenuItems = 7;
 int CursorLine = 0;
+bool locked = true;
 
 unsigned long startMillis;
 unsigned long currentMillis;
-const unsigned long period = 10000;  //the value is a number of milliseconds
+const unsigned long period = 1 * 60 * 1000; // Timeout for LCD and lock - 1 minute
 
 
-uint8_t key[] = {1, 13, 2, 14, 4, 5, 0, 7, 8, 9, 5, 12, 15, 2, 0, 1};
+uint8_t key[] = {1, 13, 2, 14, 4, 5, 0, 7, 8, 9, 5, 12, 15, 2, 0, 1}; // key for AES encryption
 char data[17];
 
 // All possible characters in a password, modified from https://wmich.edu/arts-sciences/technology-password-tips
@@ -45,13 +44,15 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  Serial.begin(9600);
-  while (!Serial);
+  //  Serial.begin(9600);
+  //  while (!Serial);
+  Serial1.begin(115200);
 
   // initialize control over the keyboard:
   Keyboard.begin();
 
-  //while (1);
+  if (locked)
+    lock_screen();
 
   randomSeed(analogRead(0));
   print_menu();
@@ -61,31 +62,59 @@ bool in_main_menu = true;
 bool main_menu_option = 0;
 
 void loop() {
+  //  if (locked) {
+  //    lock_screen();
+  //  }
   volatile unsigned char result = r.process();
-  currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  currentMillis = millis();  //get the current "time"
   if (currentMillis - startMillis >= period)  //test whether the period has elapsed
   {
-    LCDfadeOut();      //set LCD to sleep...
-  } //End if currenMillis...
+    locked = true;
+    lcd_fadeout();      // lock device and turn off LCD
+  }
 
   if (result) {
     init_backlight();  //wake up LCD...
     result == DIR_CCW ? CursorLine = CursorLine - 1 : CursorLine = CursorLine + 1;
-    Serial.println(result);
+
     if (CursorLine < 0) {
       CursorLine = MenuItems - 1; // roll over to last item
     } else if (CursorLine > MenuItems - 1) {
-      CursorLine = 0;                 // roll over to first item
+      CursorLine = 0;  // roll over to first item
     }
     print_menu();
-  } //End if result
+  }
 
   if (r.buttonPressedReleased(25)) {
+    lcd.clear();
     init_backlight();     //wake up LCD...
     lcd.setCursor(0, 0);  //(col, row)
-    lcd.print("You selected:");
+    lcd.print("Press enter to use");
     lcd.setCursor(0, 1);  //(col, row)
-    selection();
+    lcd.print("the password for:");
+    lcd.setCursor((20 - strlen(MenuLine[CursorLine])) / 2, 3);
+    lcd.print(MenuLine[CursorLine]);
+    if (CursorLine == 6) {
+      lcd.clear();
+      lcd.setCursor(0, 1);  //(col, row)
+      lcd.print("Your new Password is");
+      String new_password = generate_password();
+      lcd.setCursor(0, 2);  //(col, row)
+      lcd.print(new_password);
+      sprintf(data, "%.16s", new_password.c_str());
+      type_password();
+      Serial1.println();
+      Serial1.println();
+      Serial1.println(new_password);
+      Serial1.println();CYa%V4Amm)f^Cqrk
+      
+      delay(2000);
+
+
+      CursorLine = 0;     // reset to start position
+    } else {
+      selection();
+    }
     print_menu();
   } //endif buttonPressedReleased
 }
@@ -97,79 +126,67 @@ void print_menu()
 {
   lcd.clear();
   lcd.setCursor(0, 1);     //(col, row)
-  lcd.print("  Choose Password:  ");
+  lcd.print(" Choose a Password: ");
   lcd.setCursor(0, 2);    //2nd row
   lcd.print("<"); lcd.setCursor(columnsLCD - 1, 2); lcd.print(">");
   lcd.setCursor((20 - strlen(MenuLine[CursorLine])) / 2, 2);
   lcd.print(MenuLine[CursorLine]);
 }
 
-void selection()
-{
-  switch (CursorLine) {
-    case 0:
-      lcd.print("Option 1    ");
-      //set a flag or do something....
+void selection() {
+  while (1) {
+    if (r.buttonPressedReleased(25)) {
+      lcd.clear();
+      init_backlight();     //wake up LCD...
+      lcd.setCursor(8, 1);  //(col, row)
+      lcd.print("Done!");
+      read_password(CursorLine);
+      type_password();
       break;
-    case 1:
-      lcd.print("Option 2    ");
-      //set a flag or do something....
-      break;
-    case 2:
-      lcd.print("Option 3    ");
-      //set a flag or do something....
-      break;
-    case 3:
-      lcd.print("Option 4    ");
-      //set a flag or do something....
-      break;
-    case 4:
-      lcd.print("Option 5    ");
-      //set a flag or do something....
-      break;
-    case 5:
-      lcd.print("Option 6    ");
-      //set a flag or do something....
-      break;
-    case 6:
-      lcd.print("Option 7    ");
-      //set a flag or do something....
-      break;
-    default:
-      break;
-  } //end switch
-
-  delay(2000);
+    }
+  }
+  delay(1000);
   CursorLine = 0;     // reset to start position
-} //End selection
-
-void LCDfadeOut() {
-  //  lcd.noBacklight();
-  //  lcd.clear();
-} //End LCDfadeOut
+}
+void lcd_fadeout() {
+  lcd.noBacklight();
+  lcd.clear();
+}
 
 void init_backlight() {
   lcd.backlight();
-} //End init_backlight
+  startMillis = millis();  //initial start time
+}
 
-void typePassword(String password) {
-  // Type passwords randomly so that anybody looking at the screen cannot guess the password.
-  // -------------------------------------------------------------------
-  for (byte i = 0; i < password.length(); i++) {
-    Keyboard.print(password[i]);
-    Keyboard.print(characters[random(84)]);
-  }
-  for (byte i = 0; i < password.length(); i++) {
-    Keyboard.write(KEY_BACKSPACE);
-    Keyboard.write(KEY_LEFT_ARROW);
-  }
-  //-------------Collected from an old code of mine---------------------
 
+void lock_screen() {
+
+  while (locked) {
+    lcd.setCursor(1, 1);
+    lcd.print("Type Password or");
+    lcd.setCursor(1, 2);
+    lcd.print("verify with phone.");
+    if (Serial1.available() > 0) {
+      String received_password = Serial.readString();
+      locked = false;
+    }
+  }
+}
+
+
+
+void type_password() {
+  for (byte i = 0; i < 16; i++) {
+    Keyboard.print(data[i]);
+  }
+
+  memset(data, 0, sizeof(data));        // clear the array
+  Keyboard.write(KEY_END);
   Keyboard.write(KEY_RETURN);
 }
 
 void write_password(String password, byte slot) {   // Writes password to EEPROM with encryption
-  sprintf(data, "%.15s", password.c_str());
+  sprintf(data, "%.16s", password.c_str());
   aes128_enc_single(key, data);
 
   for (int j = slot * 16; j < (slot * 16) + 16; j++) {
@@ -189,4 +206,11 @@ void read_password(byte slot) {                   // Reads password and decrypts
     EEPROM.get(j, data[j - slot * 16]);
   }
   aes128_dec_single(key, data);
+}
+
+String generate_password() {
+  String generated_password = "";
+  for (byte i = 0; i < 16; i++)
+    generated_password += characters[random(84)];
+  return generated_password;
 }
